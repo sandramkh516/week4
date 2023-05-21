2f60bc8246fc03312c200fa3d6eac0f418f613af
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2023-05-21 21:17:38 +0430

    create react weather app

---
+++ src/Weather.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather() {
+  let [message, setMessage] = useState(false);
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setMessage(true);
+    setWeather({
+      temp: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    api = "88724523008dc9e1be18f6eb6a959b67";
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  if (message === false) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            onChange={updateCity}
+          />
+          <input type="submit" value="Search" className="button" />{" "}
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city"
+            onChange={updateCity}
+          />
+          <input type="submit" value="Search" className="button" />{" "}
+        </form>
+        <ul>
+          <li>City: {city}</li>
+          <li>Temperature: {Math.round(weather.temp)}°Ϲ</li>
+          <li>Humidity: {Math.round(weather.humidity)}%</li>
+          <li>Wind: {Math.round(weather.wind)} km/h</li>
+          <li>
+            <img src={weather.icon} alt="" />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
