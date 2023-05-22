2ed77f52d95402404107d11291f525773240624b
sandramkh516 <118845782+sandramkh516@users.noreply.github.com>
2023-05-22 18:37:05 +0430

    add link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <p>
+        This project was coded by Samaneh Mokhtari and it is <a>open-sourced</a>
+      </p>
     </div>
   );
 }
